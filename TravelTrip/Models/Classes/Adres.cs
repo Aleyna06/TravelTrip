e9a5fc5e5479d64695756bce32a57392dfbc34a2
aleyna çelik <celikaleyna71@gmail.com>
2023-02-28 14:35:46 +0300

    Code First Sınıflar bitti

---
+++ TravelTrip/Models/Classes/Adres.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTrip.Models.Classes
+{
+    public class Adres
+    {
+        [Key]
+        public int ID { get; set; }
+        public int Baslik { get; set; }
+        public int Aciklama { get; set; }
+        public int AdresAcik { get; set; }
+        public int Mail { get; set; }
+        public int Telefon { get; set; }
+        public int Konum { get; set; }
+        
+    }
+}
